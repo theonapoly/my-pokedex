8fbc43d2d8bf30585623b36ddd314b94254386d0
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-11 23:08:03 +0200

    create button previous and next + add a state (hook useState) to dynamise the render

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,26 @@ import PokemonCard from "./components/PokemonCard";
 
 function App() {
   //state (état, données)
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  console.log(pokemonIndex);
 
   //comportements (dynamique)
+  const handleClickPrev = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
 
+  const handleClickNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
   //affichage (render)
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 && <button onClick={handleClickPrev}>Précédent</button>}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={handleClickNext}>Suivant</button>
+      )}
     </div>
   );
 }
@@ -23,6 +35,21 @@ const pokemonList = [
     name: "Bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "Squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "Pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "Mew",
