9398a309f4b70409cc0cca9e93b6d26ac75b9238
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-11 19:32:41 +0200

    creat first component PokemonCard and add to App.jsx

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,21 @@
+function PokemonCard() {
+  //state (état, données)
+
+  //comportements (dynamique)
+
+  //affichage (render)
+  return (
+    <div>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt=""
+          srcset=""
+        />
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    </div>
+  );
+}
+
+export default PokemonCard;
