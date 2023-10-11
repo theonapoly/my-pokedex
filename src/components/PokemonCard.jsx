c589c0a1ba53144f34a213c83ae2e3244e7dfc8f
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-11 19:50:52 +0200

    add dynamique function + condition ternary

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,36 @@
 function PokemonCard() {
-  //state (état, données)
+  //state (état)
 
   //comportements (dynamique)
+  const pokemon = pokemonList[0];
 
   //affichage (render)
   return (
     <div>
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt=""
-          srcset=""
-        />
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>????</p>
+        )}
+        <figcaption>
+          <h1>{pokemon.name}</h1>
+        </figcaption>
       </figure>
     </div>
   );
 }
+
+// données
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+  },
+];
 
 export default PokemonCard;
